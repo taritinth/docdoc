a0f5fb96f47519763a0a417add5b7a530ea5e3fc
phawit-s <56498877+phawit-s@users.noreply.github.com>
2021-10-24 17:14:18 +0700

    Add : signup1

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Signin from "./screens/Signin";
+import Signup1 from "./screens/Signup1";
 import Signup2 from "./screens/SignUp2 ";
 
 export default function App() {
   return (
     // <Signin></Signin>
-    <Signup2></Signup2>
+    <Signup1></Signup1>
+    // <Signup2></Signup2>
   );
 }
 
---
+++ screens/Signup1.js
@@ -0,0 +1,85 @@
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Button,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
+
+export default function Signup2(props) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.signup}>Sign up</Text>
+      <Text style={styles.fullname}>Fullname</Text>
+      <TextInput style={styles.input} placeholder="Fullname"></TextInput>
+      <Text style={styles.phone}>Phone</Text>
+      <TextInput style={styles.input} placeholder="Phone"></TextInput>
+      <TouchableOpacity style={styles.button} activeOpacity={0.8}>
+        <View style={styles.buttonContainer}>
+          <Text style={styles.buttonText}>Next</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // backgroundColor:'red',
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 30,
+  },
+  signup: {
+    color: "#525252",
+    fontWeight: "bold",
+    fontSize: 50,
+    marginBottom: 50,
+    alignSelf: "flex-start",
+  },
+  input: {
+    marginTop: 10,
+    width: "100%",
+    backgroundColor: "#F6F6F6",
+    height: 50,
+    paddingVertical: 5,
+    paddingHorizontal: 30,
+    borderRadius: 30,
+  },
+  fullname: {
+    fontSize: 18,
+    marginLeft: 10,
+    color: "#595959",
+    fontWeight: "bold",
+    alignSelf: "flex-start",
+  },
+  phone: {
+    fontSize: 18,
+    marginLeft: 10,
+    color: "#595959",
+    fontWeight: "bold",
+    alignSelf: "flex-start",
+    marginTop: 25,
+  },
+
+  button: {
+    alignSelf: "stretch",
+    marginTop: 40,
+  },
+  buttonContainer: {
+    backgroundColor: "#32B5FF",
+    paddingVertical: 12,
+    paddingHorizontal: 30,
+    borderRadius: 25,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontFamily: "open-sans",
+    fontSize: 18,
+  },
+});
